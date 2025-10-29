41dec59628fa0c27938c50bf6ca243e8dd917c07
Jose Iglesias <joseiglesiasarevalo@gmail.com>
2025-10-29 11:07:39 +0100

    feat: Añadida la clase Videojuego, extiende a la clase abstracta Producto e implementa la interfaz Prestable.

--- src/main/java/biblioteca/simple/modelo/Videojuego.java
+++ src/main/java/biblioteca/simple/modelo/Videojuego.java
@@ -1,4 +1,62 @@
 package biblioteca.simple.modelo;
 
-public class Videojuego {
+import biblioteca.simple.contratos.Prestable;
+
+public class Videojuego extends Producto implements Prestable {
+
+    private String plataforma;
+    private String genero;
+    private boolean prestado;
+    private Usuario prestadoA;
+
+    public Videojuego(String titulo, String anyo, FORMATO formato, String plataforma, String genero) {
+        super(titulo, anyo, formato);
+        this.plataforma = plataforma;
+        this.genero = genero;
+    }
+
+    public Videojuego(int id, String titulo, String anyo, FORMATO formato, String plataforma, String genero) {
+        super(id, titulo, anyo, formato);
+        this.plataforma = plataforma;
+        this.genero = genero;
+    }
+
+    public String getPlataforma() {
+        return plataforma;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    @Override
+    public void prestar(Usuario usuario) {
+        if (prestado) throw new IllegalStateException("Libro ya prestado.");
+
+        this.prestado = true;
+        this.prestadoA = usuario;
+    }
+
+    @Override
+    public void devolver() {
+
+        this.prestado = false;
+        this.prestadoA = null;
+
+    }
+
+    @Override
+    public boolean estaPrestado(){return this.prestado; }
+
+    @Override
+    public String toString() {
+        return "Videojuego{" +
+                "plataforma='" + plataforma + '\'' +
+                ", genero='" + genero + '\'' +
+                ", id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", anyo='" + anyo + '\'' +
+                ", formato=" + formato +
+                '}';
+    }
 }
